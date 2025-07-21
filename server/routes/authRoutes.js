4b975aa17ecf8d262bdd02b9c2940e080c5bc701
Payashwani <Payashwanichaturvedi@gmail.com>
2025-07-21 12:50:33 +0530

    Added test route to authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const auth = require('../middleware/auth');
 
+router.get('/test', (req, res) => {
+  res.json({ message: 'Auth route working!' });
+});
+
+
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
